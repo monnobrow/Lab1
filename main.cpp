bcce6289f0f60f01ad5662c4c073a7e7365876b6
Ardod-spklv <gorskaaarina01@gmail.com>
2026-03-13 01:37:50 +0700

    love

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "triangle.h"
+#include "pryamougolnik.h"
+using namespace std;
+int main(){
+    cout<<"triangle"<<endl;
+    triangle();
+    cout<<"pryamougolnik"<<endl;
+    pryamougolnik();
+}
